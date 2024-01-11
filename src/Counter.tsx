ce193e966ef8e761d00fbf66e6d66d930a7f75ea
okuyama <130431374+okuyama-code@users.noreply.github.com>
2024-01-11 13:27:52 +0900

    useStateの動画完了

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Counter = () => {
+
+// React.FunctionComponent はReact.FCでオッケー
+// const Counter: React.FunctionComponent<{}> = () => {
+const Counter: React.FC<{}> = () => {
+  const initialValue: any = 0;
+  const [value, setValue] = useState<number>(initialValue);
+
+  // このアロー関数は、現在の状態（prevState）に1を加えて新しい状態を返すことで、setValue関数によってその新しい状態がコンポーネントのvalueにセットされます。
+  const increment = () => {
+    setValue((prevState) => prevState + 1);
+  }
+
+  const decrement = () => {
+    if (value > 0) {
+      setValue((prevState) => prevState - 1);
+    }
+  }
+
   return (
-    <div>Counter</div>
+    <div>
+      <div>value: {value}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
   )
 }
 
